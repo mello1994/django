f11409447baeb9bc6dc3c40a906629aef850c51c
mello1994 <neoenda@gmail.com>
2020-05-04 12:50:34 +0900

    changesetting

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$m%=(x8=7krd42h4v#=$+bvig3sd49f*cr3z%a66jv2k672ud-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
